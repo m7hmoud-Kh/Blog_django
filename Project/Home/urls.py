7577c450266b887180ef6745b625a6c5224dff21
mahmoud <khairymahmoud795@gmail.com>
2022-01-03 13:37:25 +0200

    First Commit

---
+++ Project/Home/urls.py
@@ -0,0 +1,17 @@
+from os import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home,name='home'),
+    path('home', views.home,name='home'),
+    path('contact',views.contact,name='contact'),
+    path('about',views.about,name='about'),
+    path('singup',views.singup,name='singup'),
+    path('singin',views.singin,name='singin'),
+    path('logout',views.logout,name='logout'),
+    path('search',views.search,name='search'),
+    path('post/<int:post_id>',views.post_single,name='single_post'),
+    path('categories_post/<int:cat_id>',views.cat_post,name='cat_post')
+
+]
